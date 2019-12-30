25cee848cef55625e68dc93e0a4618e99ba5df7a
Андрей Ефимов <efiand@ya.ru>
2019-12-30 22:42:19 +0300

    Публикация и изменение curly-spaces

--- source/js/data.js
+++ source/js/data.js
@@ -1,8 +1,8 @@
 window.data = {
   ERRORS: {
-    status: `Ошибка {{ status }}: {{ statusText }}`,
+    status: `Ошибка {{status}}: {{statusText}}`,
     connect: `Ошибка соединения. Попробуйте ещё раз.`,
-    timeout: `Запрос не успел выполниться за {{ timeout }} мс. Попробуйте ещё раз.`
+    timeout: `Запрос не успел выполниться за {{timeout}} мс. Попробуйте ещё раз.`
   },
   FIELDS: [
     `page`,
