1a841ae8d1903ae2cd96e54fb49f2ff20b5ab7f8
efiand <efiand@users.noreply.github.com>
2021-01-01 21:01:24 +0000

    deploy: eb1ea8948937b3460d1656da44e91f5d8fd86b49

--- js/scripts.js
+++ js/scripts.js
@@ -622,4 +622,26 @@ function _createClass(Constructor, protoProps, staticProps) {
       return Toc;
     }();
   applyClass(".toc", Toc);
+  if (snowfall) {
+    // eslint-disable-line
+    snowfall.start({
+      // eslint-disable-line
+      bg: "transparent",
+      primary: "#ffffff",
+      secondary: "#ffffff",
+      density: 100,
+      wave: {
+        frequency: 0.02,
+        amplitude: 2
+      },
+      gravity: {
+        angle: 90,
+        strength: 0.3
+      },
+      wind: {
+        angle: 4,
+        strength: 0
+      }
+    });
+  }
 })();
